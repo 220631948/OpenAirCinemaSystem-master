e79a9537c294b8e4bdf78f74e36aabd593a9e26b
ferdi <220631948@mycput.ac.za>
2024-05-17 17:33:59 +0200

    Test for ticket with invalid type- fail test

--- src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
@@ -1,13 +1,24 @@
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Booking;
+import za.ac.cput.domain.Ticket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PaymentFactoryTest {
+
+
+    @Test
+    public void buildTicket_withInvalidTicketType_returnsNull() {
+        Booking booking = new Booking();
+        Ticket ticket = TicketFactory.buildTicket(1, "booking1", "", "seat1", 100.0, 1, booking);
+        assertNull(ticket);
+    }
 
 }
