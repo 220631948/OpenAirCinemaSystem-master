e201108655be17b5ea0c5946eac5148b96d6084a
ferdi <220631948@mycput.ac.za>
2024-05-17 17:32:33 +0200

    Payment factory test class created

---
+++ src/test/java/za/ac/cput/factory/PaymentFactoryTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PaymentFactoryTest {
+
+}
