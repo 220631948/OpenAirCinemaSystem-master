e8b92ebe082634d8108c4b312ebbb968d00fff3a
ferdi <220631948@mycput.ac.za>
2024-05-17 17:28:17 +0200

    Added main annotations for @SpringBootTest and @TestOrder

--- src/test/java/za/ac/cput/factory/TicketFactoryTest.java
+++ src/test/java/za/ac/cput/factory/TicketFactoryTest.java
@@ -1,8 +1,14 @@
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TicketFactoryTest {
 
     @Test
