3e2ecf0fac21abe98f4b9864863f65fd96868e8f
ferdi <220631948@mycput.ac.za>
2024-05-17 17:26:49 +0200

    TicketFactory Test class created

---
+++ src/test/java/za/ac/cput/factory/TicketFactoryTest.java
@@ -0,0 +1,15 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TicketFactoryTest {
+
+    @Test
+    void buildTicket() {
+    }
+
+    @Test
+    void testBuildTicket() {
+    }
+}
