f1e6ce32789a8f37bcf9cbd319430f87ca78f625
ferdi <220631948@mycput.ac.za>
2024-05-17 17:30:12 +0200

    Different test Order Methods created.

--- src/test/java/za/ac/cput/factory/TicketFactoryTest.java
+++ src/test/java/za/ac/cput/factory/TicketFactoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.domain.Booking;
+import za.ac.cput.domain.Ticket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,10 +14,53 @@ import static org.junit.jupiter.api.Assertions.*;
 class TicketFactoryTest {
 
     @Test
-    void buildTicket() {
+    void buildTicket_withValidParameters_returnsTicket() {
+        Booking booking = new Booking();
+        Ticket ticket = TicketFactory.buildTicket(1, "booking1", "type1", "seat1", 100.0, 1, booking);
+        assertNotNull(ticket);
     }
 
     @Test
-    void testBuildTicket() {
+    void buildTicket_withInvalidTicketId_returnsNull() {
+        Booking booking = new Booking();
+        Ticket ticket = TicketFactory.buildTicket(-1, "booking1", "type1", "seat1", 100.0, 1, booking);
+        assertNull(ticket);
+    }
+
+    @Test
+    void buildTicket_withEmptyBookingId_returnsNull() {
+        Booking booking = new Booking();
+        Ticket ticket = TicketFactory.buildTicket(1, "", "type1", "seat1", 100.0, 1, booking);
+        assertNull(ticket);
+    }
+
+    @Test
+    void buildTicket_withNullBooking_returnsNull() {
+        Ticket ticket = TicketFactory.buildTicket(1, "booking1", "type1", "seat1", 100.0, 1, null);
+        assertNull(ticket);
+    }
+
+    @Test
+    void buildTicket_withExtraTicketParameter_returnsTicket() {
+        Booking booking = new Booking();
+        Ticket extraTicket = new Ticket.Builder()
+                .setTicketId(2)
+                .setBookingID("booking2")
+                .setTicketType("type2")
+                .setSeatNumber("seat2")
+                .setTicketPrice(200.0)
+                .setTicketQuantity(2)
+                .setBooking(booking)
+                .build();
+
+        Ticket ticket = TicketFactory.buildTicket(1, "booking1", "type1", "seat1", 100.0, 1, booking, extraTicket);
+        assertNotNull(ticket);
+    }
+
+    @Test
+    void buildTicket_withNullExtraTicketParameter_returnsNull() {
+        Booking booking = new Booking();
+        Ticket ticket = TicketFactory.buildTicket(1, "booking1", "type1", "seat1", 100.0, 1, booking, null);
+        assertNull(ticket);
     }
 }
